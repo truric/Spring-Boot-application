87b23dea194631542bb9cec2efc15404716ac0db
rp1122334564 <ricardo.parada@publiqare.com>
2022-06-17 17:41:28 +0100

    delete method created

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -31,4 +31,12 @@ public class StudentService {
 
         System.out.println(student); //sout
     }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("student with id " + studentId + "does not exist");
+        }
+        studentRepository.deleteById(studentId);
+    }
 }
